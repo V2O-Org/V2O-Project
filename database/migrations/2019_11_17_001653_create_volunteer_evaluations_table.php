53fde485153319f01e02fdbc38a17823eb491885
OVOWORLD <35735329+OVOWORLD@users.noreply.github.com>
2019-11-16 20:47:47 -0400

    evaluations tables

--- database/migrations/2019_11_17_001653_create_volunteer_evaluations_table.php
+++ database/migrations/2019_11_17_001653_create_volunteer_evaluations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateVolunteerEvaluationTable extends Migration
+class CreateVolunteerEvaluationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateVolunteerEvaluationTable extends Migration
      */
     public function up()
     {
-        Schema::create('volunteer_evaluation', function (Blueprint $table) {
+        Schema::create('volunteer_evaluations', function (Blueprint $table) {
             $table->bigIncrements('id');
 			$table->unsignedBigInteger('organisation_id');
 			$table->unsignedBigInteger('volunteer_id');
@@ -34,6 +34,6 @@ class CreateVolunteerEvaluationTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('volunteer_evaluation');
+        Schema::dropIfExists('volunteer_evaluations');
     }
 }
