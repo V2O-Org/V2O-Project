e32f410d4fbf81b0594237558d323923a9e03f08
geewizz246 <geewizz246@users.noreply.github.com>
2019-11-25 19:07:47 -0400

    Fixed issue with small text on some activity pages

--- resources/views/volunteer/vol-account-header.blade.php
+++ resources/views/volunteer/vol-account-header.blade.php
@@ -13,7 +13,7 @@
 		{{ Html::style('css/profile.css') }}
 
          <!--notification links-->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"> --}}
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
         
